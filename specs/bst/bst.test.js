af6181292ce4ddc0b1f4e94c297de491a375af3e
Svynct <rtkiller006@gmail.com>
2026-04-10 19:06:17 -0300

    binary search tree

--- specs/bst/bst.test.js
+++ specs/bst/bst.test.js
@@ -4,11 +4,13 @@ Binary Search Tree!
 
 Name your class Tree. 
 
-I'd suggest making another class called Node. You don't have to; you can make them all plain JS objects
+I'd suggest making another class called Node. You don't have to; you can make them all 
+plain JS objects
 
-Here you'll make a BST. Your Tree class will have keep track of a root which will be the first item added
-to your tree. From there, if the item is less than the value of that node, it will go into its left subtree
-and if greater it will go to the right subtree.
+Here you'll make a BST. Your Tree class will have keep track of a root which will be 
+the first item added to your tree. From there, if the item is less than the value of 
+that node, it will go into its left subtree and if greater it will go to the right 
+subtree.
 
 value - integer     - value being contained in the node
 left  - Node/object - the left node which itself may be another tree
@@ -18,16 +20,62 @@ right - Node/object - the right node which itself may be another tree
 
 class Tree {
   // code goes here
+  constructor() {
+    this.root = null;
+  }
+
+  add(value) {
+    if (this.root === null) {
+      this.root = new Node(value);
+    } else {
+      let current = this.root;
+      while (true) {
+        if (current.value > value) {
+          // go left
+          if (current.left) {
+            current = current.left;
+          } else {
+            current.left = new Node(value);
+            break;
+          }
+        } else {
+          // go right
+          if (current.right) {
+            current = current.right;
+          } else {
+            current.right = new Node(value);
+            break;
+          }
+        }
+      }
+    }
+    return this;
+  }
+  toJSON() {
+    return JSON.stringify(this.root.serialize(), null, 4);
+  }
+  toObject() {
+    return this.root.serialize();
+  }
 }
 
-// you might consider using a Node class too
-// class Node {
-//   // code maybe goes here
-// }
+class Node {
+  constructor(value = null, left = null, right = null) {
+    this.value = value;
+    this.left = left;
+    this.right = right;
+  }
+  serialize() {
+    const ans = { value: this.value };
+    ans.left = this.left === null ? null : this.left.serialize();
+    ans.right = this.right === null ? null : this.right.serialize();
+    return ans;
+  }
+}
 
 // unit tests
 // do not modify the below code
-describe.skip("Binary Search Tree", function () {
+describe("Binary Search Tree", function () {
   it("creates a correct tree", () => {
     const nums = [3, 7, 4, 6, 5, 1, 10, 2, 9, 8];
     const tree = new Tree();
